280a50df02a0d1e1af5f1f082efaefcbf73436a0
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-04-02 18:07:19 -0700

    update setup.py to 0.5.0 (for PyPI package update of course)

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Ashes provides a Python implementation of the dust_ templating
-language.
+Ashes provides a Python implementation of dust_, a lightweight
+templating language that makes it easy to create purely data-driven
+templates that work equally well on servers and clients.
+
+Ashes is pure Python and is tested on Python 2.7, 3.2, and PyPy.
 
 .. _dust: http://akdubya.github.com/dustjs/
 
@@ -11,20 +14,22 @@ from setuptools import setup
 
 setup(
     name='ashes',
-    version='0.4.0',
+    version='0.5.0',
     license='BSD',
     author='Mahmoud Hashemi',
     author_email='mahmoudrhashemi@gmail.com',
     url='https://github.com/mahmoud/ashes',
-    description='Dust templating for Python',
+    description='Lightweight templating for Python (a la dust)',
     long_description=__doc__,
     classifiers=(
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: JavaScript',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Text Processing :: Markup',
     ),
     py_modules=('ashes',),
