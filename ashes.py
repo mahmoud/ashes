5660f6ac2d277f7ba751c5aa1c09e52ed9d7d033
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2014-11-05 02:31:42 -0800

    version bump

--- ashes.py
+++ ashes.py
@@ -16,7 +16,7 @@ PY3 = (sys.version_info[0] == 3)
 if PY3:
     unicode, basestring = str, str
 
-__version__ = '0.7.3dev'
+__version__ = '0.7.4dev'
 __author__ = 'Mahmoud Hashemi'
 __contact__ = 'mahmoudrhashemi@gmail.com'
 __url__ = 'https://github.com/mahmoud/ashes'
